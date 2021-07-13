bcce355f1dc510fba539d48565e663e9297103e4
jaytai2040 <jayyr2040@gmail.com>
2021-07-13 22:27:24 +0800

    Food Model

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 const drinks = require("./models/drinks");
+const food = require("./models/food");
+
 
 app.get("/drinks", (req, res) => {
   // res.send(drinks)
-  res.render("index.ejs",{drinks})
+  res.render("index.ejs",{drinks,food})
   });
 
 app.get("/drinks/:id", (req, res) => {
     const pos = req.params.id;
    // res.send(pos);
-   const drink = drinks[pos];
-    res.render("show.ejs",{drink})
+    const drinkLength = drinks.length
+    res.render("show.ejs",{drinks,food,pos,drinkLength})
     });
 
 app.listen(PORT, () => {
