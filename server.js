7132fa91c18fa6d6f5c963043caff6e40aa1a006
jaytai2040 <jayyr2040@gmail.com>
2021-07-13 20:34:25 +0800

    Setting up your show route

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get("/drinks", (req, res) => {
   res.render("index.ejs",{drinks})
   });
 
+app.get("/drinks/:id", (req, res) => {
+    const pos = req.params.id;
+    res.send(pos);
+    });
+
 app.listen(PORT, () => {
     console.log("Welcome to the Gitpub App!:", PORT);
   });
