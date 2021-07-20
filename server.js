2396b161819067f351c404df98ca388f789cbfc9
jaytai2040 <jayyr2040@gmail.com>
2021-07-20 12:30:45 +0800

    first

--- server.js
+++ server.js
@@ -5,6 +5,29 @@ const PORT = process.env.PORT;
 const drinks = require("./models/drinks");
 const food = require("./models/food");
 
+/* const x = 12321;
+const strxArr = x.toString().split("");
+let countx = 0;
+for (let i = 0; i < Math.ceil(strxArr.length /2); i++){
+  if (strxArr[i] === strxArr[strxArr.length-1-i] ) {
+       countx += 1;
+  }
+}
+if (countx === Math.ceil(strxArr.length /2)){
+  console.log("its a palindrome")
+  console.log("count", countx)
+}
+else {console.log("its not")}
+console.log(strxArr);
+
+
+const y = (num) => {
+  const invY = num.toString().split("").reverse().join("");
+  return invY === num.toString().split("").join("") ? true : false
+}
+
+console.log(y(121)) */
+
 app.use(express.static('public'));
 
 app.get("/drinks", (req, res) => {
